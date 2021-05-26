5b428c2e47f1625a655fc23e7dab99078ec250ba
jot <davinder@DavindesMacBook.phub.net.cable.rogers.com>
2021-05-26 12:56:08 -0400

    Complete Final Assignment

--- A2_FA_iOS_Jaspreet_C0812273 /Model/Product+CoreDataClass.swift
+++ A2_FA_iOS_Jaspreet_C0812273 /Model/Product+CoreDataClass.swift
@@ -2,12 +2,13 @@
 //  Product+CoreDataClass.swift
 //  A2_FA_iOS_Jaspreet_C0812273
 //
-//  Created by Jaspreet Kaur on 25/05/21.
+//  Created by Jaspreetn Kaur on 25/05/21.
 //
 //
 
 import Foundation
 import CoreData
+
 
 @objc(Product)
 public class Product: NSManagedObject {
